4500f740fa6f176e218d9af33575cc53189de396
Gagan-8318 <158788916+Gagan-8318@users.noreply.github.com>
2026-03-03 12:22:47 +0530

    Delete Machine_learning directory

--- Machine_learning/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# Step 1: Import libraries
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
